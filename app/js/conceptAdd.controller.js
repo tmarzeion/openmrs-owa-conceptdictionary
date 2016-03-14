08295cf6c85974136766880dd8264ffc78fb3ad3
PawelGutkowski <pawel.gutkowski.1993@gmail.com>
2016-03-14 13:29:24 +0100

    RA-1077 fixed classeslist
    fixed refreshing of class-list
    
    fixed classes list test

---
+++ app/js/conceptAdd.controller.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+    
+	angular
+		.module('conceptDictionaryApp')
+		.controller('ConceptAddController', ConceptAddController)
+		
+	ConceptAddController.$inject = ['serverLocales', 'loadClasses', 'loadDataTypes', '$scope'];
+		
+	function ConceptAddController(serverLocales, loadClasses, loadDataTypes, $scope){
+		var vm = this;
+		
+		vm.serverLocales = serverLocales;
+		vm.classes = loadClasses;
+		vm.datatypes = loadDataTypes;
+		
+		vm.goLocale = goLocale;
+		vm.selectedClass;
+		vm.selectedDatatype;
+		vm.isSet;
+		vm.isNumeric;
+		vm.isCoded;
+		
+		vm.checkType = checkType;
+		
+		activate();
+		
+		function activate(){
+			
+		}
+		
+		function goLocale(locale){
+			
+		};
+		function checkType(){
+			vm.isNumeric = (vm.selectedDatatype === "8d4a4488-c2cc-11de-8d13-0010c6dffd0f");
+			vm.isCoded = (vm.selectedDatatype === "8d4a48b6-c2cc-11de-8d13-0010c6dffd0f");
+		}
+	};
+})();
