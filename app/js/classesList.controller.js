215032d63b6c745cbfdfebeaabb1ae01527345ed
Tomek <marzeion.tomasz@gmail.com>
2016-03-14 15:37:11 +0100

    fixed availability of delete class button

--- app/js/classesList.controller.js
+++ app/js/classesList.controller.js
@@ -15,13 +15,34 @@
         vm.classes = loadClasses;
         //map of selected classes
         vm.selected = {};
+        //checks if any is selected
+        vm.isSelected = false;
         //determines whether class has been added in previous view
         vm.classAdded = $routeParams.classAdded;
 
         vm.goTo = goTo;
         vm.deleteSelected = deleteSelected;
+        vm.refreshButtonState = refreshButtonState;
 
-       
+        function refreshButtonState () {
+
+            var foundSolution = false;
+            //for iterator?
+            var counter = 0;
+
+            angular.forEach(vm.selected, function(key) {
+                if (key) {
+                    vm.isSelected = true;
+                    foundSolution = true;
+                    //break?
+                }
+                counter++;
+            });
+            if (!foundSolution && counter == Object.keys(vm.selected).length) {
+                vm.isSelected = false;
+            }
+        }
+
         function deleteSelected (){
         	//deletes selected classes
             angular.forEach(vm.selected, function(key,value){
