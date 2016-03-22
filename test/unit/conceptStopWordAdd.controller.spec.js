10772426da7807f66bd33f8f3cb7a67e217ac5b8
Tomek <marzeion.tomasz@gmail.com>
2016-03-22 14:23:25 +0100

    RA-1070 Added Concept Stop Words page

---
+++ test/unit/conceptStopWordAdd.controller.spec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+describe('Concept dictionary controllers', function () {
+
+    beforeEach(function () {
+        jasmine.addMatchers({
+            toEqualData: function (util, customEqualityTesters) {
+                return {
+                    compare: function (actual, expected) {
+                        var passed = angular.equals(actual, expected);
+                        return {
+                            pass: passed,
+                            message: 'Expected ' + actual + (passed ? '' : ' not') + ' to equal ' + expected
+                        }
+                    }
+                }
+            }
+        });
+    });
+
+    beforeEach(module('conceptDictionaryApp'));
+
+    describe('ConceptStopWordAddController', function () {
+        var scope, ctrl, $httpBackend, location;
+
+        beforeEach(inject(function (_$httpBackend_, $rootScope, $controller, _$location_) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectPOST('/ws/rest/v1/conceptstopword')
+                .respond(
+                    {
+                        results: {
+                            value: 'AND',
+                            locale: 'en'
+                        }
+                    });
+
+            scope = $rootScope.$new();
+
+            location = _$location_;
+
+            var newConceptStopWord = {
+                value: 'AND',
+                locale: 'en'
+            };
+
+            var serverLocales =["en","es","fr","it","pt"];
+
+            ctrl = $controller('ConceptStopWordAddController', {$scope: scope, $location: _$location_, serverLocales : serverLocales});
+            ctrl.conceptStopWord = newConceptStopWord;
+        }));
+
+        it('should add new concept stop word ', function () {
+            ctrl.addConceptStopWord();
+            $httpBackend.flush();
+            expect(ctrl.success).toEqualData(true);
+        });
+    });
+});
