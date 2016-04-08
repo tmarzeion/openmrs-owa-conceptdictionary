ab6a0e3bdac62df24a63bb84beea324ca5ef9c10
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2016-04-08 14:13:09 +0200

    Fixed drug test

--- app/components/drugList/drugList.controller.spec.js
+++ app/components/drugList/drugList.controller.spec.js
@@ -36,14 +36,6 @@ describe('Concept dictionary controllers', function() {
         beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, openmrsRest){
             $httpBackend = _$httpBackend_;
 			$httpBackend.whenGET(/translation.*/).respond();
-            $httpBackend.whenGET('/ws/rest/v1/drug?v=full').
-            respond({results:[{ name: 'Morphine', 
-            					strength: 'high', 
-            					uuid: "2b22dc27-72ec-4ab5-9fa8-d98be91adc1c"},
-            				  { name: 'Drug', 
-            					strength: 'low',
-            					uuid: "3g65dc27-72ec-4ab5-9fa8-d98be91adb5b"}
-            				]});
             $httpBackend.whenGET('/ws/rest/v1/drug?includeAll=true&v=full').
             respond({results:[{ name: 'Morphine', 
             					strength: 'high', 
@@ -67,43 +59,27 @@ describe('Concept dictionary controllers', function() {
             scope = $rootScope.$new();
 
             var loadDrugs;
-            openmrsRest.listFull('drug').then(function(response){
-                loadDrugs = response;
-            });
-            var loadRetiredDrugs;
             openmrsRest.listFull('drug', {includeAll: true}).then(function(response){
-            	loadRetiredDrugs = response;
+            	loadDrugs = response;
             });
 
             $httpBackend.flush();
 
-            ctrl = $controller('DrugsListController', {$scope: scope, loadDrugs: loadDrugs, loadRetiredDrugs: loadRetiredDrugs});
+            ctrl = $controller('DrugsListController', {$scope: scope, loadDrugs: loadDrugs});
 
         }));
 
-        it('Should load non-retired drugs', function(){
-            ctrl.activate();       
-            expect(ctrl.drugsList).toEqualData([{ name: 'Morphine', 
-													strength: 'high', 
-													uuid: "2b22dc27-72ec-4ab5-9fa8-d98be91adc1c"},
-												  { name: 'Drug', 
-													strength: 'low',
-													uuid: "3g65dc27-72ec-4ab5-9fa8-d98be91adb5b"}
-												]);
-        });
-        
         it('Should load all drugs', function(){
-        	ctrl.toggleRetire();
-            expect(ctrl.drugsList).toEqualData([{ name: 'Morphine', 
-													strength: 'high', 
-													uuid: "2b22dc27-72ec-4ab5-9fa8-d98be91adc1c"},
-												  { name: 'Drug', 
-													strength: 'low',
-													uuid: "3g65dc27-72ec-4ab5-9fa8-d98be91adb5b"},
-													{ name: 'Coca', 
-													strength: 'mid',
-													uuid: "6g65dc56-72ec-4ab5-9fa8-d98be91adb5b"}
-												]);
+            expect(ctrl.drugsList).toEqualData([{ name: 'Morphine',
+                strength: 'high',
+                uuid: "2b22dc27-72ec-4ab5-9fa8-d98be91adc1c"},
+                { name: 'Drug',
+                    strength: 'low',
+                    uuid: "3g65dc27-72ec-4ab5-9fa8-d98be91adb5b"},
+                { name: 'Coca',
+                    strength: 'mid',
+                    uuid: "6g65dc56-72ec-4ab5-9fa8-d98be91adb5b"}
+            ]);
         });
 
     });
